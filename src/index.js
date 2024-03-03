d29f142de199f3b3000e5eaae80eb78fd92cc900
Nakod55 <nakodvenkatesh43@gmail.com>
2024-03-03 21:56:13 +0530

    Submit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const fs = require("fs/promises");
-const fileName = "myfile.txt";
+const fileName = "../myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+  await fs.appendFile(fileName,fileContent);
 };
-
+updateFile(fileName,",An online learning platform.");
 module.exports = updateFile;
